1aeffc1c89d1a84538731ce474ead50d592441cb
Sikandar Tahir <159277794+SikandarTahir23@users.noreply.github.com>
2024-02-15 16:47:35 +0500

    Q 05 TS/JS Of 45 Assignment
    Q 05 TS/JS Of 45 Assignment

---
+++ Q5.js
@@ -0,0 +1,5 @@
+"use strict";
+// 5. Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
+let famous_person = "Hazrat Ali R.A";
+let message = "Accept The Apology, Even If It Is Not Sincer";
+console.log(`${famous_person} Once said. “${message}”`);
